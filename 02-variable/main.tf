d93f098a25d9e6573a0cb528c17a2584a9c5c75b
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-05-21 14:38:58 +0530

    Add variable definitions and outputs in main.tf; create dev.auto.tfvars, dev.tfvars, prod.tfvars, and terrafrom.tfvars files

--- 02-variable/main.tf
+++ 02-variable/main.tf
@@ -5,3 +5,98 @@ variable "x" {
 output "x" {
   value = var.x
 }
+
+## Variable Data types
+# string
+# number
+# boolean
+# string require double quotes, number and boolean does not require quotes.
+
+variable "str" {
+  default = "Hello"
+}
+
+variable "num" {
+  default = 20
+}
+
+variable "mybool" {
+  default = true
+}
+
+# When we access the variable, irrespective of type you can access it with var.var-name
+
+# Variable requires to be in quotes if the variable is part of other characters. Also need to access that with ${var.var-nam}
+
+output "str" {
+  value = "${var.str}, Steve"
+}
+
+
+# Variable Types
+
+# Normal
+# List
+# Map / Dict
+
+variable "a" {
+  default = "THis is a normal Variable"
+}
+
+# The values in a list can comprise multiple data types
+variable "b" {
+  default = [
+    1,
+    2,
+    "abc",
+    false
+  ]
+}
+
+variable "c" {
+  default = {
+    x = 10
+    y = 20
+    z = "abc"
+  }
+}
+
+output "b1" {
+  value = var.b[0]
+}
+
+output "c1" {
+  value = var.c["x"]
+}
+
+
+## Variable from command line
+variable "cli" {
+  default = 1
+}
+
+output "command-line-variable" {
+  value = var.cli
+}
+
+## Variable from terraform.tfvars
+variable "v1" {}
+output "v1" {
+  value = var.v1
+}
+
+variable "env" {}
+output "env" {
+  value = var.env
+}
+
+
+#auto.tfvars
+variable "server_count" {}
+output "server_count" {
+  value = var.server_count
+}
+
+# terraform apply -var-file=dev.tfvars
+
+# terraform apply -var-file=prod.tfvars
